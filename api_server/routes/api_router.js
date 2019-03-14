2f7f728eda60966015d0d637f09d39661cdf1808
Liyani <liyanilustado10@gmail.com>
2019-03-14 11:30:05 -0700

    Added working authorization

--- api_server/routes/api_router.js
+++ api_server/routes/api_router.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const msgAPIController = require('../controllers/msg-api');
 const userAPIController = require('../controllers/user-api');
 
 router.route('/msgs')
 .get(msgAPIController.getAllMessagesOrderedByLastPosted)
-.post(msgAPIController.addNewMessage);
+.post(passport.authenticate('basic', { session: false}),
+ msgAPIController.addNewMessage);
 
 router.post('/users', userAPIController.registerNewUser);
 router.get('/users/login', userAPIController.loginUser);
